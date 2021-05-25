a02fb7f61d99104f102ab4a01b117e1dfc1744c3
dave_white <david.alexander.white@gmail.com>
2021-05-25 13:42:23 -0700

    add active two-way test

--- test_amm.py
+++ test_amm.py
@@ -1,5 +1,6 @@
 from amm import *
 
+
 def test_simple_uniswap():
     amm = AMM(2, 2)
     # x * y = 4, so if we put in 2 x, we should get out 1 y so that 4 * 1 = 4
@@ -8,6 +9,7 @@ def test_simple_uniswap():
     assert amm.y_reserves == 1
     assert np.allclose(res.x_out, [0.])
     assert np.allclose(res.y_out, [0.5, 0.5])
+
 
 def test_easy_two_way():
     amm = AMM(2, 3)
@@ -20,8 +22,20 @@ def test_easy_two_way():
     assert np.allclose(res.y_out, [3.])
 
 
+def test_active_two_way():
+    amm = AMM(2, 3)
+    # If we put in 2 x and 1 y, the x inflow pool gets half of y,
+    # and the y inflow pool gets 1/4 of x
+    res = amm.trade(2, 1)
+    assert amm.x_reserves == 3
+    assert amm.y_reserves == 2
+    assert np.allclose(res.x_out, [1.])
+    assert np.allclose(res.y_out, [2.])
+
+
 
 
 if __name__ == "__main__":
     test_simple_uniswap()
     test_easy_two_way()
+    test_active_two_way()
