c180455793e77263d5323795fd46372649d479f1
dave_white <david.alexander.white@gmail.com>
2021-05-25 14:12:02 -0700

    added whale order class

---
+++ test_whale_order.py
@@ -0,0 +1,27 @@
+from whale_order import *
+
+
+def test_whale_order():
+    order = WhaleOrder(8, 2)
+
+    assert order.qty_in_per_block == 4
+
+    order.update_after_block(3)
+    assert order.blocks_left == 1
+    assert order.qty_filled == 3
+
+    order.update_after_block(2)
+    assert order.blocks_left == 0
+    assert order.qty_filled == 5
+
+    check_works = False
+    try:
+        order.update_after_block(2)
+    except AssertionError:
+        check_works = True
+    assert check_works
+
+
+if __name__ == "__main__":
+    test_whale_order()
+
---
+++ whale_order.py
@@ -0,0 +1,18 @@
+class WhaleOrder:
+    def __init__(self, qty_in, blocks_lifespan):
+        self.qty_in = qty_in
+        self.blocks_lifespan = blocks_lifespan
+        self.qty_in_per_block = qty_in / blocks_lifespan
+
+        # Ongoing stats
+        self.blocks_left = blocks_lifespan
+        self.qty_filled = 0
+
+    def update_after_block(self, qty_filled):
+        assert self.is_live()
+
+        self.qty_filled += qty_filled
+        self.blocks_left -= 1
+
+    def is_live(self):
+        return self.blocks_left > 0
