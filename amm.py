6592a0b7abdb0345880ad9d6a8e6e7f74d7b62e0
dave_white <dave@paradigm.xyz>
2021-06-08 11:22:30 -0700

    switch to using new diffeq formulation from whitepaper

--- amm.py
+++ amm.py
@@ -45,12 +45,13 @@ class AMM:
         if np.sum(x_in) == 0 or np.sum(y_in) == 0:
             return self.trade(x_in, y_in)
 
-        l = np.sqrt(self.x_reserves * self.y_reserves)
-        sqrt_p_ratio = np.sqrt(np.sum(y_in) / np.sum(x_in))
-        c = (1 - self.y_reserves / (l * sqrt_p_ratio)) / (1 + self.y_reserves / (l * sqrt_p_ratio))
-        end_y_sum = sqrt_p_ratio * l * (np.exp(2 * np.sum(x_in) * sqrt_p_ratio / l) - c) / (
-                np.exp(2 * np.sum(x_in) * sqrt_p_ratio / l) + c)
-        end_x_sum = l ** 2 / end_y_sum
+        k = self.x_reserves * self.y_reserves
+        c = (self.x_reserves*np.sqrt(y_in)-np.sqrt(k*np.sum(x_in))) /\
+            (self.x_reserves*np.sqrt(y_in)+np.sqrt(k*np.sum(x_in)))
+        exponent = 2*np.sqrt(np.sum(x_in)*np.sum(y_in)/k)
+        end_x_sum = np.sqrt(k*np.sum(x_in)/np.sum(y_in))*(np.exp(exponent)+c)/\
+            (np.exp(exponent)-c)
+        end_y_sum = k/end_x_sum
 
         x_out_sum = self.x_reserves - end_x_sum + np.sum(x_in)
         y_out_sum = self.y_reserves - end_y_sum + np.sum(y_in)
